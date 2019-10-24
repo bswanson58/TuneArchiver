d768434c5be5f0c5d7b910d4c467e44925412043
Bill Swanson <bswanson58@gmail.com>
2019-10-24 10:33:25 -0500

    Initial project, a copy of ForRent.

---
+++ TuneArchiver/ApplicationModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using TuneArchiver.Interfaces;
+using TuneArchiver.Logging;
+using TuneArchiver.Platform;
+
+namespace TuneArchiver {
+    class ApplicationModule : IModule {
+        public void RegisterTypes( IContainerRegistry containerRegistry ) {
+            containerRegistry.Register<IEnvironment, OperatingEnvironment>();
+            containerRegistry.Register<IFileWriter, JsonObjectWriter>();
+            containerRegistry.Register<IPlatformLog, SeriLogAdapter>();
+            containerRegistry.Register<IPreferences, PreferencesManager>();
+        }
+
+        public void OnInitialized( IContainerProvider containerProvider ) {
+        }
+    }
+}
